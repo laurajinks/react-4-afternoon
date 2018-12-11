bc939f6f19c44f9ed07352aca0ee778c764db045
Laura <laura.jinks@gmail.com>
2018-12-11 14:45:13 -0600

    Finish through Black Diamond steps

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+
+    this.state = {
+      students: []
+    } 
+  }
+
+  componentDidMount () {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+    .then(response => this.setState({students: response.data}))
+    .catch(err => console.log(err))
   }
 
   render() {
+    const students = this.state.students.map((student, i) => 
+    <Link to={`/student/${student.id}`} key={i}><h3>{student.first_name} {student.last_name}</h3></Link>);
+                          
     return (
       <div className="box">
         <h1></h1>
         <h2>ClassList:</h2>
+        {students}
+        <Link to='/'><button className='btn'>Back</button></Link>
 
       </div>
     )
